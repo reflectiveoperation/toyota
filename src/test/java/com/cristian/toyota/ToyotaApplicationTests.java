a6da20566a5f097e583dd81ce6a34260a116cbf1
Cristian Alvarez <cristian.alvarez@bglgroup.co.uk>
2023-08-05 16:05:24 +0100

    First commit

---
+++ src/test/java/com/cristian/toyota/ToyotaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristian.toyota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyotaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
